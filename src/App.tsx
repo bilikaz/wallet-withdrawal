062691300aef18db8a8ee1af44e6186037ec096a
Valdas Barakauskas <valdas@vbtech.eu>
2022-02-18 19:46:07 +0000

    init

---
+++ src/App.tsx
@@ -0,0 +1,350 @@
+import React, { Component, Fragment } from 'react';
+import Web3 from "web3";
+import Web3Modal from "web3modal";
+import { getChainData } from "./helpers/utilities";
+import supportedChains from "./helpers/config";
+import './App.css';
+
+interface AppState {
+  subscribeProvider: boolean
+  fetching: boolean
+  address: string
+  chainBalance: string
+  balance: string
+  web3: any
+  provider: any
+  connected: boolean
+  chainId: number
+  networkId: number
+  networkSupported: boolean
+  pendingWithdrawal: boolean
+  withdrawalAmount: number
+  withdrawalAddress: string
+  validWithdrawalAddress: boolean
+  allowance: number
+}
+
+const INITIAL_STATE: AppState = {
+  subscribeProvider: true,
+  fetching: false,
+  address: "",
+  chainBalance: "0",
+  balance: "0",
+  web3: null,
+  provider: null,
+  connected: false,
+  chainId: 1,
+  networkId: 1,
+  networkSupported: false,
+  pendingWithdrawal: false,
+  withdrawalAmount: 0,
+  withdrawalAddress: "",
+  validWithdrawalAddress: false,
+  allowance: 0
+}
+
+class App extends Component { 
+
+  public  web3Modal: Web3Modal
+  public  state: AppState
+  private contract: any
+  private token: any
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      ...INITIAL_STATE
+    };
+
+    this.web3Modal = new Web3Modal({
+      cacheProvider: true,
+      providerOptions: {}
+    });
+  }
+
+  componentDidMount() {
+    if (this.web3Modal.cachedProvider) {
+      this.connectWallet();
+    }
+  }
+
+  connectWallet = async () => {
+    const provider = await this.web3Modal.connect()
+    await this.subscribeProvider(provider)
+    await provider.enable()
+    const web3: any = new Web3(provider)
+    await this.setState({
+      web3,
+      provider,
+      connected: true,
+    });
+    this.onConnect();
+  }
+
+  onConnect = async () => {
+    const { web3 } = this.state;
+    const accounts = await web3.eth.getAccounts()
+    const address = accounts[0]
+    const networkId = await web3.eth.net.getId()
+    const chainId = await web3.eth.getChainId()
+    const chainData = getChainData(chainId)
+    const networkSupported = !!chainData.wallet_address.length && !!chainData.token_address.length
+    if ( await web3.eth.net.isListening() && networkSupported ) {
+      this.contract =
+        new web3.eth.Contract(
+          chainData.wallet_abi,
+          chainData.wallet_address
+        )
+      this.token =
+        new web3.eth.Contract(
+          chainData.token_abi,
+          chainData.token_address
+        )
+    } else {
+      this.contract = {}
+      this.token = {}
+    }
+    await this.setState({
+      address,
+      chainId,
+      networkId,
+      networkSupported
+    });
+    await this.getAccountAssets();
+  };
+
+  public resetApp = async () => {
+    const { web3 } = this.state;
+    if (web3 && web3.currentProvider && web3.currentProvider.close) {
+      await web3.currentProvider.close();
+    }
+    await this.web3Modal.clearCachedProvider();
+    this.setState({ ...INITIAL_STATE, subscribeProvider: false })
+  }
+
+  public subscribeProvider = async (provider: any) => {
+    const { subscribeProvider } = this.state;
+    
+    if (!provider.on || !subscribeProvider) { return }
+
+    provider.on("close", () => this.resetApp())
+    provider.on("accountsChanged", () => this.onConnect())
+    provider.on("chainChanged", () => this.onConnect())
+
+    await this.setState({
+      subscribeProvider : false
+    });
+  };
+
+  public async switchNetwork(chainId: number) {
+    const { web3 } = this.state;
+    let chainData: any
+    
+    try {
+      chainData = getChainData(chainId)
+    } catch (error:any) {
+      this.resetApp()
+      return
+    }
+    
+    try {
+      await (web3.currentProvider! as any).request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: web3.utils.toHex(chainData.chain_id) }],
+      });
+    } catch (error:any) {
+      if (error.code === 4902) {
+        try {
+          await (web3.currentProvider! as any).request({
+            method: "wallet_addEthereumChain",
+            params: [
+              {
+                chainId: web3.utils.toHex(chainData.chain_id),
+                chainName: chainData.name,
+                rpcUrls: [chainData.rpc_url],
+                nativeCurrency: {
+                  name: chainData.native_currency.name,
+                  symbol: chainData.native_currency.symbol,
+                  decimals: Number(chainData.native_currency.decimals),
+                }
+              },
+            ],
+          });
+        } catch (error:any) {
+          alert('Unable to add new network: '+chainData.name+'. Please try to add this network manually.');
+          console.log(error.message)
+        }
+      }
+    }
+  }
+
+  public getAccountAssets = async () => {
+    const { web3, address, chainId, networkSupported } = this.state;
+    const chainData = getChainData(chainId)
+    this.setState({ fetching: true });
+    if ( networkSupported ) {
+      try {
+        await this.token.methods.balanceOf(address).call().then( async (balance: string) => {
+          await this.setState({ 
+            fetching: false, 
+            balance: Number(balance) > 0 ? web3.utils.toBN( balance ).div(
+              web3.utils.toBN( 10 ).pow( web3.utils.toBN( String( chainData.native_currency.decimals ) ) )
+            ).toString() : balance
+          })
+        } )
+      } catch (error) {
+        console.error(error);
+        await this.setState({ fetching: false });
+      }
+    } else {
+      await this.setState({ balance: "0" });
+    }
+    web3.eth.getBalance(address).then( (balance: any) => {
+      this.setState({ chainBalance: web3.utils.fromWei(balance, 'ether') });
+    } )
+  }
+
+  public async handleWithdrawalChanged(event:any) {
+    await this.setState({
+      withdrawalAmount: Number(event.target.value),
+    });
+  }
+
+  public async handleWithdrawalAddressChanged(event:any) {
+    const { web3 } = this.state;
+    await this.setState({
+      withdrawalAddress: String(event.target.value),
+      validWithdrawalAddress: web3.utils.isAddress(event.target.value)
+    });
+  }
+
+  public async toggleSubmit() {
+    this.setState({ pendingWithdrawal: !this.state.pendingWithdrawal })
+  }
+
+  public async handleWithdrawal() {
+    const { web3, address, chainId, networkSupported, withdrawalAddress, validWithdrawalAddress } = this.state;
+    const chainData = getChainData(chainId)
+    if( networkSupported && validWithdrawalAddress ) {
+      try {
+        this.setState({ fetching: true });
+        // Call internal API to get user identifier (e.g. withdrawal_id)
+        // This mockup expects withdrawal_id returned if funds are available with number greater than 0
+        // If API returns 0 this would mean the funds are not available
+        // Please edit validation logic to your liking
+        // Only a mockup here
+        const call_api = async (): Promise<number> => {
+          return new Promise<number>((resolve) => {
+            resolve(1234)
+          })
+        }
+        call_api().then(async (withdrawal_id: number) => {
+          if ( withdrawal_id > 0 ) {
+            await this.contract.methods.request(
+              withdrawal_id,
+              withdrawalAddress,
+              web3.utils.toBN( this.state.withdrawalAmount ).mul(
+                web3.utils.toBN( 10 ).pow( web3.utils.toBN( String( chainData.native_currency.decimals ) ) )
+              ).toString() 
+            ).send({from: address})
+            // Set some success state here
+            this.setState({ 
+              fetching: false,
+              withdrawalAmount: 0
+            })
+          } else {
+            // Set some Error message here
+            console.log('API RETURNED INSUFICIENT FUNDS')
+            this.setState({})
+          }
+        } )
+      } catch (error) {
+        console.error(error);
+        // Set some Error message here
+        await this.setState({ fetching: false });
+      }
+      await this.getAccountAssets()
+    }
+    
+  }
+
+  public render = () => {
+    const {
+      address,
+      connected,
+      chainId,
+      fetching,
+      pendingWithdrawal,
+      withdrawalAmount,
+      withdrawalAddress,
+      networkSupported,
+      balance,
+      chainBalance,
+      allowance,
+    } = this.state;
+
+    const chainData = getChainData(chainId)
+
+    const renderWalletButton = (
+      connected ? (
+        <button value="" onClick={this.resetApp}>Disconnect Metamask</button>
+      ) : (
+        <button value="" onClick={this.connectWallet}>Connect Metamask</button>
+      )
+    )
+
+    const renderFormButton = (
+      pendingWithdrawal ? (
+        <Fragment>
+          <button onClick={()=>this.toggleSubmit()}>Back</button>
+          <button onClick={()=>this.handleWithdrawal()}>Submit</button> 
+        </Fragment>
+      ) : (
+       <button onClick={()=>this.toggleSubmit()}>Next</button>  
+      )
+    )
+
+    const renderFormInput = (
+      pendingWithdrawal ? (
+        <div>
+          Chain: {chainData.chain} <br/>
+          Amount: {withdrawalAmount} <br/>
+          Address: {withdrawalAddress} <br/>
+        </div>
+      ) : (
+        <Fragment>
+          <input type="number" value={withdrawalAmount} onChange={this.handleWithdrawalChanged.bind(this)}/>
+          <input type="text" value={withdrawalAddress} onChange={this.handleWithdrawalAddressChanged.bind(this)}/>
+        </Fragment>
+      )
+    )
+
+    const renderChains = supportedChains.map(chain => (
+      <button className={`chainbutton ${chainId == chain.chain_id ? "active" : ""}`} value="" onClick={()=>this.switchNetwork(chain.chain_id)}>{chain.name}</button>
+    ))
+
+    return (
+      <div className="App">
+        {renderWalletButton}<br/><br/>
+        {connected && renderChains}
+        <div>
+          {networkSupported
+            ? <div>
+                <span>Network supported.</span><br/>
+                <span>Contract address: {chainData.wallet_address}</span><br/>
+                <span>Token address: {chainData.token_address}</span><br/>
+                <span>Token balance: {balance}</span><br/>
+              </div>
+            : <div>
+                <span>Network unsupported.</span><br/>
+              </div>
+          }
+        </div>
+        { (networkSupported && connected) && renderFormInput }
+        { (networkSupported && connected) && renderFormButton }
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/helpers/config.ts
@@ -0,0 +1,149 @@
+import { AbiItem } from 'web3-utils'
+
+export interface ChainData {
+  name: string;
+  short_name: string;
+  chain: string;
+  network: string;
+  chain_id: number;
+  network_id: number;
+  rpc_url: string;
+  wallet_address: string;
+  wallet_abi: AbiItem[];
+  token_address: string;
+  token_abi: AbiItem[];
+  token_approve: number;
+  native_currency: {
+    symbol: string;
+    name: string;
+    decimals: string;
+  };
+}
+
+const supportedChains: ChainData[] = [
+  {
+    name: "Ethereum Mainnet",
+    short_name: "eth",
+    chain: "ETH",
+    network: "mainnet",
+    chain_id: 1,
+    network_id: 1,
+    rpc_url: "https://mainnet.infura.io/v3/%API_KEY%",
+    wallet_address: "",
+    wallet_abi: [],
+    token_address: "",
+    token_abi: [],
+    token_approve: 0,
+    native_currency: {
+      symbol: "ETH",
+      name: "Ethereum",
+      decimals: "18",
+    }
+  },
+  {
+    name: "Ethereum Ropsten",
+    short_name: "rop",
+    chain: "ETH",
+    network: "ropsten",
+    chain_id: 3,
+    network_id: 3,
+    rpc_url: "https://ropsten.infura.io/v3/%API_KEY%",
+    wallet_address: "0x43ab74575C7b1Ec16F8bD8757960E133827aB8Cc",
+    wallet_abi: [
+      {"constant":false,"name":"request","inputs":[{"internalType":"uint256","name":"__withdrawalId","type":"uint256"},{"internalType":"address","name":"__recipient","type":"address"},{"internalType":"uint256","name":"__amount","type":"uint256"}],"outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}
+    ],
+    token_address: "0xffFb75282e5d10780afce094794A42e6CBB428a7",
+    token_abi: [
+      {"constant":true,"name":"name","inputs":[],"outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},
+      {"constant":false,"name":"approve","inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},
+      {"constant":true,"name":"decimals","inputs":[],"outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},
+      {"constant":true,"name":"balanceOf","inputs":[{"name":"_owner","type":"address"}],"outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},
+      {"constant":true,"name":"symbol","inputs":[],"outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},
+      {"constant":true,"name":"allowance","inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"}
+    ],
+    token_approve: 0,
+    native_currency: {
+      symbol: "ETH",
+      name: "Ethereum",
+      decimals: "18",
+    }
+  },
+  {
+    name: "Ethereum Rinkeby",
+    short_name: "rin",
+    chain: "ETH",
+    network: "rinkeby",
+    chain_id: 4,
+    network_id: 4,
+    rpc_url: "https://rinkeby.infura.io/v3/%API_KEY%",
+    wallet_address: "",
+    wallet_abi: [],
+    token_address: "",
+    token_abi: [],
+    token_approve: 0,
+    native_currency: {
+      symbol: "ETH",
+      name: "Ethereum",
+      decimals: "18"
+    }
+  },
+  {
+    name: "Binance Smart Chain",
+    short_name: "bsc",
+    chain: "smartchain",
+    network: "mainnet",
+    chain_id: 56,
+    network_id: 56,
+    rpc_url: "https://bsc-dataseed.binance.org/",
+    wallet_address: "",
+    wallet_abi: [],
+    token_address: "",
+    token_abi: [],
+    token_approve: 0,
+    native_currency: {
+      symbol: "BNB",
+      name: "BNB",
+      decimals: "18"
+    }
+  },
+  {
+    name: "Polygon",
+    short_name: "polygon",
+    chain: "polygon",
+    network: "mainnet",
+    chain_id: 137,
+    network_id: 137,
+    rpc_url: "https://polygon-rpc.com/",
+    wallet_address: "",
+    wallet_abi: [],
+    token_address: "",
+    token_abi: [],
+    token_approve: 0,
+    native_currency: {
+      symbol: "MATIC",
+      name: "MATIC",
+      decimals: "18"
+    }
+  },
+  {
+    name: "Avalanche Network",
+    short_name: "avax",
+    chain: "avax",
+    network: "mainnet",
+    chain_id: 43114,
+    network_id: 43114,
+    rpc_url: "https://api.avax.network/ext/bc/C/rpc",
+    wallet_address: "",
+    wallet_abi: [],
+    token_address: "",
+    token_abi: [],
+    token_approve: 0,
+    native_currency: {
+      symbol: "AVAX",
+      name: "AVAX",
+      decimals: "18"
+    }
+  },
+];
+
+export default supportedChains;
